d082120d6dbf64bf9b99416865f03d154031b741
Erik Timmers <e.timmers@gmail.com>
2014-07-10 16:39:29 +0200

    Moto G - unlock bootloader shell script

--- scripts/moto-g-unlock-bootloader.sh
+++ scripts/moto-g-unlock-bootloader.sh
@@ -1,44 +1,45 @@
-#!/bin/bash
+#!/bin/sh
 
 fastbootCheck=`fastboot devices`
 
 if [ -z "$fastbootCheck" ]; then
-	echo "Put your device in fastboot mode"
-	echo "(power off, then press the power and"
-	echo "  volume down buttons simultaneously)"
+	echo "Put your device in fastboot mode (i.e. power off, and then"
+	echo "press the power and volume down buttons simultaneously)."
 	exit 1
 fi
 
 unlockData=`fastboot oem get_unlock_data 2>&1`
-unlockData=$(echo $unlockData | sed "s/ (bootloader) //g") # remove '(bootloader)'
-unlockData=$(echo $unlockData | sed "s/\.//g") # remove (leading) dots
-unlockKey=$(echo $unlockData | grep -o "^\S*") # cut at whitespace
+unlockKey=`echo ${unlockData} | sed "s/ (bootloader) //g" | sed "s/\.//g" | grep -o "^\S*"`
 
-phoneSN=$(echo $unlockKey | awk -F'#' '{print $1}')
-phonePUID=$(echo $unlockKey | awk -F'#' '{print $4}')
-phoneHash=$(echo $unlockKey | awk -F'#' '{print $3}')
+phoneSN=`echo ${unlockKey} | awk -F'#' '{print $1}'`
+phonePUID=`echo ${unlockKey} | awk -F'#' '{print $4}'`
+phoneHash=`echo ${unlockKey} | awk -F'#' '{print $3}'`
 
 url="https://motorola-global-portal.custhelp.com/cc/productRegistration/unlockPhone/$phoneSN/$phonePUID/$phoneHash/"
 
-echo "First log in at https://motorola-global-portal.custhelp.com/app/standalone/bootloader/unlock-your-device-a if you havn't already"
+echo "First log in at the Motorola website if you have not already:"
 echo ""
-echo "Now visit $url"
+echo "  https://motorola-global-portal.custhelp.com/app/standalone/bootloader/unlock-your-device-a"
+echo ""
+echo "Now visit:"
+echo ""
+echo "  $url"
 echo ""
 echo "Or use the unlock key: $unlockKey"
 echo ""
 
-fastboot reboot-bootloader
+fastboot reboot-bootloader > /dev/null 2>&1
 
 echo ""
 echo -n "Enter the unlock code: "
 read unlockCode
 echo ""
-
 echo "About to run"
-echo "  fastboot oem unlock $unlockCode"
+echo ""
+echo "  $ fastboot oem unlock $unlockCode"
 echo ""
 echo -n "Do you wish to continue? (y/n) "
-read -n 1 confirm
+read confirm
 echo ""
 
 if [ "$confirm" != "y" ]; then
